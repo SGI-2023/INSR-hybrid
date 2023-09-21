f987cf9ce3f2ea9f1359dd23705035e2a5157745
jpvt <joaoteixeira@eng.ci.ufpb.br>
2023-09-21 16:42:35 -0300

    added sum of multiple gaussians example

--- advection/examples.py
+++ advection/examples.py
@@ -8,6 +8,10 @@ def get_examples(src, mu=-1.5, **kwargs):
         source_func = partial(gaussian_like, mu=mu)
     elif src == 'example1_grad':
         source_func = partial(gaussian_gradient, mu=mu)
+    elif src == 'example2':
+        source_func = partial(gaussian_sum, mu=[-1.5, -1.0, -0.5], sigma=[0.1, 0.2, 0.1])
+    elif src == 'example2_grad':
+        source_func = partial(gaussian_sum_gradient, mu=[-1.5, -1.0, -0.5], sigma=[0.1, 0.2, 0.1])
     else:
         raise NotImplementedError
     return source_func
@@ -20,3 +24,12 @@ def gaussian_gradient(x, mu=0, sigma=0.1):
     """Gradient of the normalized gaussian distribution with respect to x"""
     return -((x - mu) * torch.exp(-0.5 * (x - mu) ** 2 / sigma ** 2)) / sigma ** 2
 
+def gaussian_sum(x, mu=[0], sigma=[0.1]):
+    """Sum of normalized gaussian distributions"""
+    assert len(mu) == len(sigma)
+    return sum([gaussian_like(x, mu[i], sigma[i]) for i in range(len(mu))])
+
+def gaussian_sum_gradient(x, mu=[0], sigma=[0.1]):
+    """Gradient of the sum of normalized gaussian distributions with respect to x"""
+    assert len(mu) == len(sigma)
+    return sum([gaussian_gradient(x, mu[i], sigma[i]) for i in range(len(mu))])
--- scripts/grid_search_experiments_siren.sh
+++ scripts/grid_search_experiments_siren.sh
@@ -1,28 +1,24 @@
-for i_omega0 in 5 30 
+for i_omega0 in 30 
 do
-    for i_n_width in  128 256
+    for i_n_width in  256
     do 
-        for i_hidden_layers in   5 6
+        for i_hidden_layers in   6
         do
-            for i_learning_rate in 0.00001 0.0001
+            for i_learning_rate in 0.0001
             do
-                for i_rel_w in 10 0.1 0.01
-                do
-                    python main.py advection \
-                        --tag gridsearch_test_advect1D_siren_omega0_${i_omega0}_width_${i_n_width}_hidden_${i_hidden_layers}_lr_${i_learning_rate}_relw_${i_rel_w}\
-                        --init_cond example1 \
-                        --num_hidden_layers ${i_hidden_layers} \
-                        --hidden_features ${i_n_width} \
-                        --network siren \
-                        -sr 5000 \
-                        --max_n_iters 20000 \
-                        --dt 0.05 \
-                        -T 0 \
-                        --lr ${i_learning_rate}\
-                        -g 0 \
-                        --omega_0 ${i_omega0} \
-                        --rel_w ${i_rel_w}
-                done
+                python main.py advection \
+                    --tag advect1D_sumfreq_omega0_${i_omega0}_width_${i_n_width}_hidden_${i_hidden_layers}_lr_${i_learning_rate}\
+                    --init_cond example2 \
+                    --num_hidden_layers ${i_hidden_layers} \
+                    --hidden_features ${i_n_width} \
+                    --network siren \
+                    -sr 5000 \
+                    --max_n_iters 5000 \
+                    --dt 0.05 \
+                    -T 30 \
+                    --lr ${i_learning_rate}\
+                    -g 0 \
+                    --omega_0 ${i_omega0} 
             done
         done
     done
